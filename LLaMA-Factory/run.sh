c854bed7e63dde4973a96604f3797108711e0fd4
yangweiqing <143774034+ywq2001@users.noreply.github.com>
2024-08-05 22:04:50 +0800

    Add files via upload

---
+++ LLaMA-Factory/run.sh
@@ -0,0 +1,27 @@
+CUDA_VISIBLE_DEVICES=2,3  llamafactory-cli train \
+    --stage sft \
+    --do_train \
+    --model_name_or_path MODEL_PATH \
+    --dataset  your_data_name\
+    --dataset_dir LLaMA-Factory/data \
+    --template llama3 \
+    --finetuning_type lora \
+    --lora_target all \
+    --output_dir  Your_output_path\
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --cutoff_len 2048 \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 1 \
+    --warmup_steps 30 \
+    --save_steps 100 \
+    --save_strategy "epoch" \
+    --evaluation_strategy "no" \
+    --learning_rate 2e-5 \
+    --num_train_epochs 1.0 \
+    --val_size 0 \
+    --plot_loss \
+    --fp16
