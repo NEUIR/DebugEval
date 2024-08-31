5e5dd4e4706318e4e88939ceff72f6fb8a28e654
yangweiqing <143774034+ywq2001@users.noreply.github.com>
2024-09-01 03:50:48 +0800

    Update code_rep_calculate_acc.py

--- code_rep_calculate_acc.py
+++ code_rep_calculate_acc.py
@@ -17,6 +17,43 @@ question_error = {}
 
 dict = {}
 
+def estimate_pass_at_k(num_samples, num_correct, k):
+    """Estimates pass@k of each problem and returns them in an array."""
+
+    def estimator(n: int, c: int, k: int) -> float:
+        """Calculates 1 - comb(n - c, k) / comb(n, k)."""
+        if n - c < k:
+            return 1.0
+        return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
+
+    import itertools
+
+    if isinstance(num_samples, int):
+        num_samples_it = itertools.repeat(num_samples, len(num_correct))
+    else:
+        assert len(num_samples) == len(num_correct)
+        num_samples_it = iter(num_samples)
+
+    return np.array(
+        [estimator(int(n), int(c), k) for n, c in zip(num_samples_it, num_correct)]
+    )
+
+def calculate_pass_at_k(results, k_list=[1, 5]):
+    # Calculate pass@k.
+    total, correct = [], []
+    for result in results:
+        passed = [result]
+        total.append(len(passed))
+        correct.append(sum(passed))
+    total = np.array(total)
+    correct = np.array(correct)
+
+    ks = k_list
+    pass_at_k = {f"pass@{k}": estimate_pass_at_k(total, correct, k).mean()
+                 for k in ks if (total >= k).all()}
+    # print(pass_at_k)
+    return pass_at_k
+
 def read_jsonl_file(path):
     data = []
     with open(path,'r',encoding='utf8') as f:
@@ -48,11 +85,10 @@ for i,line in enumerate(results):
         dict[error].append(0)
 
 
-print(dict)
-print(f'average:{np.mean(acc)}')
-print(dict.keys())
-print(f'syntax:{np.mean(dict["syntax error"])}')
-print(f'reference:{np.mean(dict["reference error"])}')
-print(f'logic:{np.mean(dict["logic error"])}')
-print(f'multiple:{np.mean(dict["multiple error"])}')
+
+print(f'average:{calculate_pass_at_k(acc)}')
+print(f'syntax:{calculate_pass_at_k(dict["syntax error"]))}')
+print(f'reference:{calculate_pass_at_k(dict["reference error"])}')
+print(f'logic:{calculate_pass_at_k(dict["logic error"])}')
+print(f'multiple:{calculate_pass_at_k(dict["multiple error"])}')
 
